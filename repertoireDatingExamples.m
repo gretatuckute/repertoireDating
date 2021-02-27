9148841a810ffd945c4d4be2768f59a7fb29e596
Greta Tuckute <gretatu@mit.edu>
2021-02-26 19:02:35 -0500

    rep dating

--- repertoireDatingExamples.m
+++ repertoireDatingExamples.m
@@ -28,35 +28,117 @@
     % Dataspace on Github: <a href="matlab:web('https://github.com/skollmor/dspace', '-browser')">https://github.com/skollmor/dspace</a>
 
 
-%% Create random data
-nEpochs = 40;                                          % number of epochs (e.g. days)
-nSubEpochs = 5;                                        % number of sub epochs  (e.g. periods in day)
-nSamples = 250;                                        % samples per subEpoch
-dim = 25;                                              % data dimension
+%% Try to load real KNN data
+data_super = load('/Users/gt/Documents/GitHub/neural_manifolds/local/result/feb26/NN-tree_nclass=64_nobj=64000_nhier=6_beta=0.000161_sigma=5.0_nfeat=936-train_test-fixed_layer_2_Linear_numSubsamples_100_k_100.mat')
+
+%%
+hier_num = 6;
+data = data_super.super_struct.hier_6;
+NNids_data = data.NNids;
+
+% fix normalization 
+NNids_t = size(NNids,1)*NNids;
+NNids = ceil(NNids_t);
+
+figure;imagesc(NNids)
+
+%% Get params
+nSamples = 100;                                        % samples per subEpoch
+nEpochs = 10;                                          % number of epochs (e.g. days)
+nSubEpochs = length(NNids)/nSamples/10;                                        % number of sub epochs  (e.g. periods in day)
 N = nEpochs * nSubEpochs * nSamples;                   % total number of datapoints
 productionTime = (1:N)';                               % production time for each datapoint
 epoch = ceil(productionTime/(nSubEpochs*nSamples));
 subEpoch = floor(mod(productionTime-1, (nSubEpochs*nSamples))/nSamples) + 1;
-data = randn(N, dim) + (1:N)'/N * 2;
+permute = False
+
+%% Create random data
+% nEpochs = 40;                                          % number of epochs (e.g. days)
+% nSubEpochs = 5;                                        % number of sub epochs  (e.g. periods in day)
+% nSamples = 250;                                        % samples per subEpoch
+% dim = 25;                                              % data dimension
+% N = nEpochs * nSubEpochs * nSamples;                   % total number of datapoints
+% productionTime = (1:N)';                               % production time for each datapoint
+% epoch = ceil(productionTime/(nSubEpochs*nSamples));
+% subEpoch = floor(mod(productionTime-1, (nSubEpochs*nSamples))/nSamples) + 1;
+% data = randn(N, dim) + (1:N)'/N * 2;
+% permute = false
 
 %% Compute k-NN graph
-NNids = knnsearch(data, data, 'K', 100);               % or use your preferred nearest neighbor searcher 
-NNids = NNids(:, 2:end);                               % points should not be their own neighbors
+
+if permute
+    data1 = data;
+    rand_idx = randperm(length(data1));
+    data = data1(rand_idx, :);
+end 
+
+NNids_self = knnsearch(data, data, 'K', 100);               % or use your preferred nearest neighbor searcher 
+NNids = NNids_self(:, 2:end);                               % points should not be their own neighbors
+
+%% GT norm computation (prob not relevant)
+% normNN = zeros(100 - 1, length(epoch));
+% 
+% for i=2:100
+%     normNN(i-1, :)=(abs(NNids_self(:,1) - NNids_self(:,i)));%.^2;
+% end
+% 
+% meanNormNN = mean(normNN,1);
+% stdNormNN = std(normNN,1)
 
 %% Compute and plot repertoire dating pecentiles 
 [RPD, RPD_epoch, RPD_subEpoch] = repertoireDating.percentiles(NNids, epoch, subEpoch);
-repertoireDating.plotPercentiles(RPD, RPD_epoch, RPD_subEpoch, 16:25);
+
+%%
+repertoireDating.plotPercentiles(RPD, RPD_epoch, RPD_subEpoch, 3:5);
+
+%%
+RP = repertoireDating.renditionPercentiles(NNids, epoch, 'percentiles', 50);
 
 %% Compute and plot rendition percentiles for all datapoints from epoch 20
-repertoireDating.renditionPercentiles(NNids, epoch, 'valid', epoch == 1, 'doPlot', true);
+repertoireDating.renditionPercentiles(NNids, epoch, 'valid', epoch == 3, 'doPlot', true);
     
 %% Compute mixing matrix and plot it
 MM = repertoireDating.mixingMatrix(NNids, epoch, 'doPlot', true);
 
+%% Plot percentiles
+NNids=ceil(data_super.super_struct.hier_1.NNids*N);
+figure;
+plot(prctile(NNids,[5,50,95],2))
+
+figure;
+plot(prctile(NNids,[5,50,95],2))
+
+
+figure;plot(movmean(prctile(NNids,[5,50,95],2),10));
+
+%% Across different hierarchies
+
+NNids = ceil(data_super.super_struct.hier_1.NNids*N); 
+figure;plot(movmean(prctile(NNids,[5,50,95],2),10));title('Hierarchy 1')
+
+NNids = ceil(data_super.super_struct.hier_2.NNids*N); 
+figure;plot(movmean(prctile(NNids,[5,50,95],2),10));title('Hierarchy 2')
+
+NNids = ceil(data_super.super_struct.hier_3.NNids*N); 
+figure;plot(movmean(prctile(NNids,[5,50,95],2),10));title('Hierarchy 3')
+
+NNids = ceil(data_super.super_struct.hier_4.NNids*N); 
+figure;plot(movmean(prctile(NNids,[5,50,95],2),10));title('Hierarchy 4')
+
+NNids = ceil(data_super.super_struct.hier_6.NNids*N); 
+figure;plot(movmean(prctile(NNids,[5,50,95],2),10));title('Hierarchy 6')
+
 %% Compute stratified mixing matrix and plot it
 RP = repertoireDating.renditionPercentiles(NNids, epoch, 'percentiles', 50);
 % RP is the repertoire time (50th percentile)
-stratMM = repertoireDating.stratifiedMixingMatrices(data, epoch, subEpoch, RP, 'uEpochs', 16:25);
+
+%% MDS
+MM = repertoireDating.mixingMatrix(NNids, subEpoch, 'doPlot', true, 'doMDS', true);
+
+
+
+% fix this
+stratMM = repertoireDating.stratifiedMixingMatrices(data, epoch, subEpoch, RP, 'uEpochs', 1:5);
 % Avg the stratified mixing matrices
 C = arrayfun(@(i) stratMM.allMMs{i}.log2CountRatio, 1:numel(stratMM.allMMs), 'uni', false);
 avgStratMM = nanmean(cat(3, C{:}), 3);
